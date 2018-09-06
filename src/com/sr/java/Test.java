31e867b3614873d7ef8a8d2948162df329a753c3
tom_glb <cj_glb@cj.com>
2018-09-06 20:38:49 +0800

    Initial commit

---
+++ src/com/sr/java/Test.java
@@ -0,0 +1,11 @@
+package com.sr.java;
+
+public class Test {
+    public static final String a="aaaa";
+
+    public static void main(String[] args) {
+        System.out.println("Test.main");
+        System.out.println("args = [" + args + "]");
+    }
+
+}
